864417f29e6249a49d8fc3a8711181cdc5fb6038
DaveParillo <DaveParillo@users.noreply.github.com>
2019-08-14 21:36:31 -0700

    fix compile flags

--- common.cmake
+++ common.cmake
@@ -1,12 +1,20 @@
-set (CMAKE_CXX_STANDARD_REQUIRED 14)
+set (CMAKE_CXX_STANDARD 14)
+set (CMAKE_CXX_STANDARD_REQUIRED YES)
+set (CMAKE_CXX_EXTENSIONS OFF)
+
+if (MSVC)
+  add_compile_options(/W4)
+else()
+  add_compile_options(-Wall -Wextra -pedantic)
+endif(MSVC)
 
 include (CTest)
 
 if(NOT EXISTS "${CMAKE_SOURCE_DIR}/extern/doctest.h")
-  file(DOWNLOAD https://raw.githubusercontent.com/onqtam/doctest/2.3.3/doctest/doctest.h
+  file(DOWNLOAD https://raw.githubusercontent.com/onqtam/doctest/2.3.4/doctest/doctest.h
     "${CMAKE_SOURCE_DIR}/../extern/doctest.h"
     STATUS DOWNLOAD_STATUS
-    EXPECTED_HASH MD5=f37c2fdc39c7d8eb7d2dbf7a039a37df
+    EXPECTED_HASH MD5=413803ccfbf0e29acbc0885028c27fe0
     )
   message(STATUS " doctest download status: ${DOWNLOAD_STATUS}")
 endif()
