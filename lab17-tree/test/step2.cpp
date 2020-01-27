4dcd3d8a3955dbc03f73e58e097d18ff5973dd0d
DaveParillo <DaveParillo@users.noreply.github.com>
2020-01-26 20:43:39 -0800

    add lab17-tree

---
+++ lab17-tree/test/step2.cpp
@@ -0,0 +1,34 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_NO_POSIX_SIGNALS
+
+#include <string>
+
+#include <doctest.h>
+#include "../src/lab17.h"
+
+SCENARIO( "Test tree(T)") {
+
+  WHEN( "mesa::tree(T) is declared with an int type" ) {
+    mesa::tree<int> actual;
+    actual.value = 8;
+
+    THEN( "the tree can be assigned a value" ) {
+      REQUIRE(actual.value == 8);
+      CHECK_MESSAGE(actual.left == nullptr, "Left child is not the nullptr");
+      CHECK_MESSAGE(actual.right == nullptr, "Left child is not the nullptr");
+    }
+  }
+
+  WHEN( "mesa::make_tree(T) is declared with a double type" ) {
+    mesa::tree<double> actual;
+    actual.value = 2.781828;
+    THEN( "the tree can be assigned a value" ) {
+      REQUIRE(actual.value == 2.781828);
+      CHECK_MESSAGE(actual.left == nullptr, "Left child is not the nullptr");
+      CHECK_MESSAGE(actual.right == nullptr, "Left child is not the nullptr");
+    }
+  }
+
+}
+
+
