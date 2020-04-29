d1685357d51871976c4cee3ec24a8644241e0942
DaveParillo <DaveParillo@users.noreply.github.com>
2020-04-29 14:27:48 -0700

    fix bool check for buffy

--- lab19-array/test/step2.cpp
+++ lab19-array/test/step2.cpp
@@ -14,17 +14,17 @@ SCENARIO( "Test mesa::array<T,N>") {
     THEN( "the array compiles" ) {
       CHECK_MESSAGE(&actual == &actual, "placeholder for compilation");
     } AND_THEN ( "the value_type is 'int'" ) {
-      CHECK_MESSAGE( (std::is_same<mesa::array<int,1>::value_type, int>()), 
+      CHECK_MESSAGE( (std::is_same<mesa::array<int,1>::value_type, int>() == true), 
           "expected 'value_type' to be 'int'");
     }
   }
 
   WHEN( "mesa::array is declared with other types" ) {
     THEN ( "the value_type could be 'double'" ) {
-      CHECK_MESSAGE( (std::is_same<mesa::array<double,1>::value_type, double>()), 
+      CHECK_MESSAGE( (std::is_same<mesa::array<double,1>::value_type, double>() == true), 
           "expected 'value_type' to be 'double'");
     } AND_THEN ( "the value_type could be 'char'" ) {
-      CHECK_MESSAGE( (std::is_same<mesa::array<char,1>::value_type, char>()), 
+      CHECK_MESSAGE( (std::is_same<mesa::array<char,1>::value_type, char>() == true), 
           "expected 'value_type' to be 'char'");
     }
   }
