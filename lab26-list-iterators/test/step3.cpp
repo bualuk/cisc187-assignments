e9b82d61fca85be98354fd8769986e6a16aa96e6
DaveParillo <DaveParillo@noreply.github.com>
2020-05-21 14:59:07 -0700

    comment out copy ctr and assignment tests

--- lab26-list-iterators/test/step3.cpp
+++ lab26-list-iterators/test/step3.cpp
@@ -25,39 +25,39 @@ SCENARIO( "Test mesa::list_iterator<T> constructors") {
     }
   }
 
-  WHEN( "mesa::list_iterator is copy constructed" ) {
-    auto node = new mesa::list_node<char>('x');
-    mesa::list_iterator<char> original(node);
-    mesa::list_iterator<char> copy(original);
-    THEN ( " the list_node value is stored in both iterators " ) {
-      CHECK( original.node->value == node->value); 
-      CHECK( original.node->value == copy.node->value); 
-    }
-    AND_THEN ( " a change to the copy does not change the original " ) {
-      copy.node->value = 'a';
-      CHECK( original.node->value == 'x'); 
-    }
-  }
+  // WHEN( "mesa::list_iterator is copy constructed" ) {
+  //   auto node = new mesa::list_node<char>('x');
+  //   mesa::list_iterator<char> original(node);
+  //   mesa::list_iterator<char> copy(original);
+  //   THEN ( " the list_node value is stored in both iterators " ) {
+  //     CHECK( original.node->value == node->value); 
+  //     CHECK( original.node->value == copy.node->value); 
+  //   }
+  //   AND_THEN ( " a change to the copy does not change the original " ) {
+  //     copy.node->value = 'a';
+  //     CHECK( original.node->value == 'x'); 
+  //   }
+  // }
 
-  GIVEN( "2 list_iterators" ) {
-    auto node = new mesa::list_node<char>('x');
-    auto tmp = new mesa::list_node<char>('t');
-    mesa::list_iterator<char> original(node);
-    mesa::list_iterator<char> copy(tmp);
-    CHECK( 't' == copy.node->value); 
+  // GIVEN( "2 list_iterators" ) {
+  //   auto node = new mesa::list_node<char>('x');
+  //   auto tmp = new mesa::list_node<char>('t');
+  //   mesa::list_iterator<char> original(node);
+  //   mesa::list_iterator<char> copy(tmp);
+  //   CHECK( 't' == copy.node->value); 
 
-    WHEN( "a list_iterator is copy assigned to another" ) {
-      copy = original;
-      THEN ( " the list_node value is stored in both iterators " ) {
-        CHECK( original.node->value == node->value); 
-        CHECK( original.node->value == copy.node->value); 
-      }
-      AND_THEN ( " a change to the copy does not change the original " ) {
-        copy.node->value = 'a';
-        CHECK( original.node->value == 'x'); 
-      }
-    }
-  }
+    // WHEN( "a list_iterator is copy assigned to another" ) {
+    //   copy = original;
+    //   THEN ( " the list_node value is stored in both iterators " ) {
+    //     CHECK( original.node->value == node->value); 
+    //     CHECK( original.node->value == copy.node->value); 
+    //   }
+    //   AND_THEN ( " a change to the copy does not change the original " ) {
+    //     copy.node->value = 'a';
+    //     CHECK( original.node->value == 'x'); 
+    //   }
+    // }
+  // }
 
 }
 
