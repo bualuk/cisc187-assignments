65ea9f696a520a7311d24f1acff7825204304a94
bualuk <69828552+bualuk@users.noreply.github.com>
2020-08-19 21:42:36 -0700

    Update change.cpp

--- lab02-review/src/change.cpp
+++ lab02-review/src/change.cpp
@@ -4,7 +4,7 @@ purchase_t buy_stuff ()
 {
   purchase_t purchase;
 
-  for (double price = 0.10; purchase.funds >= price; price += 0.10) 
+  for (double price = 1; purchase.funds >= price; price += 1) 
   {
     purchase.funds -= price;
     purchase.items += 1;
